3c53ab3a5f135b40aa03df29942168f9f3d7b453
Oleg Sitnikov <o.sitnikov@pr-solution.ru>
2019-01-18 16:40:12 +0300

    init

---
+++ app/src/main/kotlin/ru/apptimizm/testcoroutinesandretrofit/MainActivity.kt
@@ -0,0 +1,31 @@
+package ru.apptimizm.testcoroutinesandretrofit
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.util.concurrent.TimeUnit
+
+class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        GlobalScope.launch(Dispatchers.Main) {
+            Log.e("TEST","Hello it has started")
+            delay(10000L)
+            textView.text = "I have chaned after 10 sec"
+        }
+
+    }
+
+
+}
